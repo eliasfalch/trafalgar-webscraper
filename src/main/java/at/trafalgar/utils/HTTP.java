94320ab986faed001e5ceafb490cedfe5d209aee
Elias <42185401+eliasfalch@users.noreply.github.com>
2025-07-24 05:08:28 +0200

    Update HTTP.java

--- src/main/java/at/trafalgar/utils/HTTP.java
+++ src/main/java/at/trafalgar/utils/HTTP.java
@@ -16,12 +16,12 @@ public class HTTP {
     public static boolean isReachable(String urlString) {
         try {
             URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("HEAD");
-            conn.setConnectTimeout(3000);
-            conn.setReadTimeout(3000);
-            int code = conn.getResponseCode();
-            conn.disconnect();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.setConnectTimeout(3000);
+            connection.setReadTimeout(3000);
+            int code = connection.getResponseCode();
+            connection.disconnect();
             return code >= 200 && code < 400;
         } catch (Exception e) {
             return false;
